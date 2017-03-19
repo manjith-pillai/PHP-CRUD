66a941da62580aa314cd8f971418671f44defe41
manjithcb <manjith_103@hotmail.com>
2017-03-19 16:57:37 +0530

    whole first commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP - CRUD</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="pull-left">
+					<h2>PHP CRUD</h2>
+				</div>
+				<div class="pull-right" style="margin-top: 20px">
+					<button type="button" class="btn btn-success" data-toggle="modal" data-target="#create-item"> Create Item</button>
+				</div>	
+			</div>
+		</div>
+
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>Title</th>
+					<th>Description</th>
+					<th width="200px">Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				
+			</tbody>
+		</table>
+
+		<ul class="pagination" class="pagination-sm"></ul>
+
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+
+
+<!-- Create Item Modal -->
+<div class="modal fade" id="create-item" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Create Item</h4>
+      </div>
+      <div class="modal-body">
+                <form data-toggle="validator" action="api/create.php" method="POST">
+        	<div class="form-group">
+        		<label class="control-label" for="title">Title:</label>
+					<input type="text" name="title" class="form-control" data-error="Please enter title." required />
+					<div class="help-block with-errors"></div>
+        	</div>
+        	<div class="form-group">
+					<label class="control-label" for="title">Description:</label>
+							<textarea name="description" class="form-control" data-error="Please enter description." required></textarea>
+							<div class="help-block with-errors"></div>
+			</div>
+			<div class="form-group">
+					<button type="submit" class="btn crud-submit btn-success">Submit</button>
+			</div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Edit Item Modal -->
+<div class="modal fade" id="edit-item" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Edit Item</h4>
+      </div>
+      <div class="modal-body">
+			<form data-toggle="validator" action="api/update.php" method="put">
+		      			<input type="hidden" name="id" class="edit-id">
+
+		      			<div class="form-group">
+							<label class="control-label" for="title">Title:</label>
+							<input type="text" name="title" class="form-control" data-error="Please enter title." required />
+							<div class="help-block with-errors"></div>
+						</div>
+
+						<div class="form-group">
+							<label class="control-label" for="title">Description:</label>
+							<textarea name="description" class="form-control" data-error="Please enter description." required></textarea>
+							<div class="help-block with-errors"></div>
+						</div>
+
+						<div class="form-group">
+							<button type="submit" class="btn btn-success crud-submit-edit">Submit</button>
+						</div>
+
+		      		</form>
+      </div>
+     
+    </div>
+  </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.0.slim.min.js"
+  integrity="sha256-qLAv0kBAihcHZLI3fv3WITKeRsUX27hd6upBBa0MSow="
+  crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.4.1/jquery.twbsPagination.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/validator/7.0.0/validator.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+   <script>
+   	
+   </script>
+</body>
+</html>
+
